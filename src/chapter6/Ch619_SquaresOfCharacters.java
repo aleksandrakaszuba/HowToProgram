85387608a74f28ead6d951cae599e024fa8cfe91
onberry <onberry@gmail.com>
2017-09-15 17:11:44 -0400

    * Exercise 6.19

--- src/chapter6/Ch619_SquaresOfCharacters.java
+++ src/chapter6/Ch619_SquaresOfCharacters.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 /**
  * Created by ola on 9/10/17.
+ * Java. How to program P&H Deitel
+ * Exercise 6.19
+ * Prints a square on the screen out of a char chosen by user
  */
 public class Ch619_SquaresOfCharacters {
 
@@ -13,16 +16,13 @@ public class Ch619_SquaresOfCharacters {
                 System.out.print(fillCharcter);
             }
             System.out.println();
-
         }
-
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter side of square:");
         int side = s.nextInt();
-        System.out.println("Enter temperature:");
         char fill = s.next().charAt(0);
         displaySquare(side, fill);
 
